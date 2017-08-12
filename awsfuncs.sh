548c76b5eb1a15c015192e0865263b57ec7f72a1
George Jones <gmj@pobox.com>
2017-08-12 14:07:52 -0400

    "be" 2 change id, link2home.sh, anaconda

--- awsfuncs.sh
+++
@@ -1,9 +0,0 @@
-awsRmAll () {
-    # Remove all objects in an AWS bucket
-    #
-      aws s3 ls s3://$1 | sed 's/.*4 //' | sed 's/^/aws s3 rm s3:\/\/'$1'\//' | tee /dev/tty | cat
-}
-
-#
-
-alias awsrmall=awsRmAll
