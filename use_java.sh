3320840e2d37764517d37bd27c3fd33cead6d53c
George Jones <gmj@pobox.com>
2019-10-25 08:01:07 -0400

    more robust error handling in use_java.sh

--- use_java.sh
+++ use_java.sh
@@ -1,2 +1,23 @@
+# source this.
 # Set Java HOME per location of javac
-[ ! -z "$JAVA_HOME" ] || export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
+
+# Helper functions
+PROG=`basename "$0" | tr -d '\n'`
+
+function info()  { echo ${PROG}\: info: "$@" 1>&2; }
+function warn()  { echo ${PROG}\: warning: "$@" 1>&2; }
+function error() { echo ${PROG}\: error: "$@" 1>&2; }
+function debug() { [[ -v DEBUG ]] && echo ${PROG}\: debug: "$@" 1>&2 || true ; }
+function die()   { echo ${PROG}\: fatal: "$@" 1>&2 && exit 1; }
+
+
+if [ -z "$JAVA_HOME" ]; then
+    JAVA_PATH=$(which javac)
+
+    if [ -z "$JAVA_PATH" ]; then
+        unset JAVA_PATH
+        warn "javac not installed."
+    else
+        export JAVA_HOME=$(dirname $(dirname $(readlink -f "$JAVA_PATH")))
+    fi
+fi
