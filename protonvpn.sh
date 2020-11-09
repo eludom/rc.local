47d577dc281d9342b1b77e30bf0f0ae28e775083
George Jones <gmj@pobox.com>
2020-11-09 05:52:55 -0500

    no warning defining pvc funcs

--- protonvpn.sh
+++ protonvpn.sh
@@ -1,7 +1,7 @@
 # aliases for use with protonvpn
 
 if which protonvpn; then
-    info defining protonvpn aliases
+    #info defining protonvpn aliases
 
     alias pvc='sudo protonvpn c -f && ping -c 3 8.8.8.8 && speedtest'
     alias pvd='sudo protonvpn d'
